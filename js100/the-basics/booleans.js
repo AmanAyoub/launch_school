c4bfad77c54d2790cfbe18ed045051f459507ca3
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-03 22:28:18 +0430

    exercises

---
+++ js100/the-basics/booleans.js
@@ -0,0 +1,6 @@
+// Boolean is a datatype that returns either of two values i.e. true or false.
+
+console.log(50 === 50) // returns true
+console.log(49 === 50) // returns false
+console.log('Aman' === 'aman') // case is different returns false
+console.log('Aman' === 'Aman') // returns true

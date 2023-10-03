76698ee63a38f55f52a0c5082e08eac21c8ec092
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-03 22:39:11 +0430

    exercises

---
+++ js100/the-basics/undefined.js
@@ -0,0 +1,11 @@
+// The undefined property indicates that a variable has not been assigned a value, or not declared at all.
+// We can also explicitly use the literal undefined.
+
+let fruit;
+console.log(fruit); // returns undefined
+
+let grade = undefined;
+console.log(grade); // returns undefined
+
+let team = ['Ajmal', 'Ahmad', undefined, 'Aman'];
+console.log(team[2]); // returns undefined

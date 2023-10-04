7f62acdf6d0b672f5155fd78cdf1f102e568a29b
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-04 06:35:56 +0430

    arithmetic some basic operators

---
+++ js100/arithmetic-operations/basic-operators.js
@@ -0,0 +1,16 @@
+// Use the ( + ) operator to perform addition:
+console.log(20 + 20); // returns 40
+
+// Use the ( - ) operator to perform subtraction:
+console.log(20 - 5); // returns 15
+
+// Use the ( * ) operator to perform multiplication:
+console.log(5 * 6); // returns 30
+
+// To divide two numbers—integers or decimals—with the ( / ) operator:
+console.log(30 / 5); // returns 6
+console.log(18 / 2.5); // returns 7.2
+
+// The remainder ( % ) operator returns the remainder left over 
+// when one operand is divided by a second operand.
+console.log(25 % 4); // returns 1

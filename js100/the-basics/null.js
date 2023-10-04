e8a90ede9f4d97bc5df98bebf96e19008078c2f2
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-04 06:08:30 +0430

    exercises

---
+++ js100/the-basics/null.js
@@ -0,0 +1,4 @@
+// null is a special value that represents an empty or unknown value.
+
+let number = null // The code suggests that the number variable is empty at the moment and may have a value later.
+

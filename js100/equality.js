db7a3339239749e5542d7f4e9227aaf5a8bb4bdc
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-05 10:00:33 +0430

    equality comparison

---
+++ js100/equality.js
@@ -0,0 +1,7 @@
+// The equality ( == ) operator checks whether its two operands are equal, 
+// returning a Boolean result.
+
+console.log(30 === 30); // returns true
+console.log(30 === 40); // returns false
+console.log("Ishu" === "Isheo"); // returns false
+console.log('ishu' === 'ishu'); // returns true

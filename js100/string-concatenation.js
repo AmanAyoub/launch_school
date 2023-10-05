a6a164b0c8658e11093acd732310e4a3ce34ca7c
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-05 10:10:45 +0430

    string concatenation

---
+++ js100/string-concatenation.js
@@ -0,0 +1,6 @@
+// String Concatenation means appending one or more strings to the end of another string.
+// We can do so with ( + ) operator.
+
+console.log('Aman ' + 'Ayoub'); // returns Aman Ayoub
+console.log('Foot' + 'ball'); // returns Football
+console.log('1' + '9'); // since both operands are strings so it returns 19

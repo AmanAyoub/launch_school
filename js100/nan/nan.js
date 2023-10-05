d08a984d5ec9e3000778dbc784d7365b3c18def4
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-05 09:49:11 +0430

    update

--- js100/nan/nan.js
+++
@@ -1,7 +0,0 @@
-// NaN stands for Not a Number. 
-// It represents a value that is not a valid number. 
-// It can be used to check whether a number entered is a valid number or not a number.
-
-// Some operations, such as 0 / 0, return the value NaN:
-console.log(0 / 0); // NaN
-console.log(typeof NaN); // number

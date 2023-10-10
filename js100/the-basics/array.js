87241c4e83d1abbdfbbc2013fe7f0ad539559058
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-10 21:32:40 +0430

    array structure

---
+++ js100/the-basics/array.js
@@ -0,0 +1,21 @@
+// Array is a single variable that is used to store elements of different data types.
+// The structure of the array is the same which is enclosed between two square brackets [ ].
+// The elements of the array will be separated by a comma (, ).
+
+
+// This array contains 4 elements of strings.
+let array = [
+'Tom Hanks', 
+'Johnny Depp', 
+'Will Smith',
+'Tom Cruise'
+];
+
+
+// We can access an array element by placing its index inside brackets ([]).
+// Element indexes are non-negative integers starting with 0, that's why it returns the first element
+console.log(array[0]); // returns Tom Hanks
+
+
+console.log(array[2]); // returns Will Smith
+

d21426ad35426fb0fa70dbff88b798aa56fa21a5
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 11:09:14 +0430

    using functions

---
+++ js100/functions/function.js
@@ -0,0 +1,12 @@
+// most languages have a structure called a procedure that lets you extract the code and run it as a separate unit. 
+// In JavaScript, we call these procedures functions.
+
+// Here's a function named sayBye:
+function sayBye() {
+	console.log("Bye Aman");
+}
+
+// To access the function:
+sayBye(); // returns Bye Aman
+
+

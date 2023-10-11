eb942fb72ab98dc65df7e2796832ff6f9d7ff2df
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 15:20:15 +0430

    paramenters and arguments

---
+++ js100/functions/para-arg.js
@@ -0,0 +1,12 @@
+// A parameter is one of the variables in a function. 
+// And when a method is called, the arguments are the data you pass into the method's parameters.
+
+// Create a function that says hello
+function sayHi(name) { /* name is a parameter or an empty variable it takes value as an argument 
+	while invoking the function*/
+	console.log(`Hello, ${name}`);
+}
+
+sayHi(Aman); // returns Hello, Aman
+
+

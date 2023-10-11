8db8f5d5352976c55fee4d20e5545a9db825b458
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 10:40:34 +0430

    exercises for the variables chapter

---
+++ js100/variables/exercises.js
@@ -0,0 +1,19 @@
+// Write a program named greeter.js that greets 'Victor' three times. 
+// Your program should use a variable and not hard code the string value 'Victor' in each greeting.
+let name = 'Victor';
+console.log(`Good morning ${name}`); // returns Good morning Victor
+console.log(`Good afternoon ${name}`); // returns Good afternoon Victor
+console.log(`Good evening ${name}`); // returns Good evening Victor
+
+// Write a program named age.js that includes someone's age 
+// and then calculates and reports the future age in 10, 20, 30 and 40 years.
+let age = 18;
+console.log(`You are ${age} years old.\nIn 10 years you will be ${age + 10} years old.\nIn 20 years you will be ${age + 20} years old.`);
+console.log(`In 30 years you will be ${age + 30} years old.\nIn 40 years you will be ${age + 40} years old.`);
+
+// returns:
+// You are 18 years old.
+// In 10 years, you will be 28 years old.
+// In 20 years, you will be 38 years old.
+// In 30 years, you will be 48 years old.
+// In 40 years, you will be 58 years old.

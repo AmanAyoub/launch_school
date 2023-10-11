c99ebdeb225f2341c12bf55ff01ee36e469d29b5
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 11:17:00 +0430

    the output

---
+++ js100/input-output/output.js
@@ -0,0 +1,4 @@
+// console.log, this built-in function takes any JavaScript value, regardless of type, and logs it to the console.
+
+let name = 'Aman'
+console.log(`Good morning ${name}`); // returns Good morning Aman

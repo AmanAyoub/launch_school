e9e2a503635319be54c4d4fe7d50757d7ff84cf6
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 15:25:51 +0430

    default value for the parameters

---
+++ js100/functions/default.js
@@ -0,0 +1,9 @@
+// The default parameter is a way to set default values 
+// for function parameters when a value is no passed in as an argument.
+
+function sayHi(name = 'Aman') {
+	console.log(`Hello, ${name}`);
+}
+
+sayHi('Sahil'); // returns Hello, Sahil
+sayHi(); // returns Hello, Aman

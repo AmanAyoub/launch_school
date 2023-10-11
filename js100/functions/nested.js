cc9acc6772d7a88e56775f6ee8ec6cfd3dd64bb3
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 15:20:45 +0430

    nested functions

---
+++ js100/functions/nested.js
@@ -0,0 +1,13 @@
+// We can create functions anywhere, even nested inside another function
+// But we can access to it only inside that function:
+
+function game() {
+	function football() {
+		console.log(`Let's play football`);
+	}
+	football();
+}
+game(); // returns Let's play football
+
+// We cannot access nested function:
+football(); // ReferenceError: football is not defined

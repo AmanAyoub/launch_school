812fe0f7b825cf0493df6c8e179f8bd51c8df50a
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 15:25:21 +0430

    some mathmatical operating functions

---
+++ js100/functions/function-composition.js
@@ -0,0 +1,13 @@
+// We can define some simple functions that does combination:
+function devide(num1, num2) {
+	return num1 / num2
+}
+console.log(devide(25, 5)); // returns 5
+console.log(devide(100, 5)); // returns 20
+
+function subtract(num1, num2) {
+	return num1 - num2
+}
+console.log(subtract(12, 6)); // returns 6
+console.log(subtract(25, 5)); // returns 20
+

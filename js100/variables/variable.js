a1ae9568656058e5d0cc6cc117a4d7159148128d
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 10:02:31 +0430

    declaring variables

---
+++ js100/variables/variable.js
@@ -0,0 +1,13 @@
+// A variable is simply a name of storage location, we can store value in that location.
+// Variable that doesn't have a specific value it initializes with a value of undefined.
+
+let movie
+console.log(movie); // returns undefined
+
+// To assign a more useful value, we can reassign the value of movie:
+movie = 'Cast Away'
+console.log('My favorite movie is ' + movie); // returns My favorite movie is Cast Away
+
+
+
+

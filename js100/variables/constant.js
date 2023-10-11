8f1ac811f10167c396243f42a41a60384b965bfa
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 10:10:06 +0430

    variables with const keyword

---
+++ js100/variables/constant.js
@@ -0,0 +1,8 @@
+// The const keyword is similar to let.
+// We cannot reassign the value of variables declared with const keyword.
+
+const age = 18;
+console.log(age); // returns 18
+
+age = 19 // returns TypeError
+

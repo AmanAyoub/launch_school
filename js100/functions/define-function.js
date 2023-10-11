3b3aa891f0d6a3b3a09ee5a750cb61d239aa0f52
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-11 15:26:24 +0430

    defining functions in 3 ways

---
+++ js100/functions/define-function.js
@@ -0,0 +1,22 @@
+// There are 3 ways to define a function.
+
+// Function Declaration:
+function greet() {
+	console.log(`Hello, good morning`);
+}
+greet(); // returns Hello, good morning
+
+
+// Function Expression:
+let sayHi = function () {
+	console.log('Hi');
+}
+sayHi(); // returns Hi
+
+
+// Arrow Function:
+let sayBye = () => {
+	console.log('Bye');
+}
+sayBye(); // returns Bye
+

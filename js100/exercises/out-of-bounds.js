51833d948e16e6e058960add18d51bda2be5ae69
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-15 16:36:07 +0430

    accessing unavailable array element

---
+++ js100/exercises/out-of-bounds.js
@@ -0,0 +1,4 @@
+// If we try to access an unavailable array element, it returns undefined:
+
+let netflix = ['The Terminal', 'Cast Away', 'Trees of Peace'];
+console.log(netflix[8]); // returns undefined

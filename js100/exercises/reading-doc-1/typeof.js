1b0dd5ee20f8e83220b012f81d11006a94aeb77a
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-17 15:18:01 +0430

    typeof operator

---
+++ js100/exercises/reading-doc-1/typeof.js
@@ -0,0 +1,10 @@
+// Typeof in JavaScript is an operator used for type checking 
+// and returns the data type of the operand passed to it;
+let value = 'Aman Ayoub';
+console.log(typeof value); // returns string
+
+value = 50;
+console.log(typeof value); // returns number
+
+value = true;
+console.log(typeof value); // returns boolean

cbaf9bd4ac052ea36eb42be084435a982ff5bf66
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-17 14:49:43 +0430

    accessing an array specific element

--- js100/exercises/lowercase.js
+++
@@ -1,4 +0,0 @@
-// The toLowerCase() method converts a string to lower case letters:
-
-let string = 'Aman Ayoub';
-console.log(string.toLowerCase()); // returns aman ayoub
--- js100/exercises/out-of-bounds.js
+++
@@ -1,4 +0,0 @@
-// If we try to access an unavailable array element, it returns undefined:
-
-let netflix = ['The Terminal', 'Cast Away', 'Trees of Peace'];
-console.log(netflix[8]); // returns undefined

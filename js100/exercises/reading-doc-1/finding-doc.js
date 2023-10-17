2bf34ed53abc8e846d392513a59ca6c4f64fd633
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-17 14:52:22 +0430

    where to search for JS docs

--- js100/exercises/reading-doc-1/finding-doc.js
+++ js100/exercises/reading-doc-1/finding-doc.js
@@ -5,3 +5,4 @@ The MDN web docs is an excellent resource for web developers,
 specially it is one of the best sources of documentation on JavaScript. 
 The site includes guides, tutorials, reference, tools and resources for JavaScript objects.
 */
+

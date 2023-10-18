2b1d40564e3e0ae9a6edc2927509544c3866ff2f
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-18 20:26:21 +0430

    equality & strict equality

---
+++ js100/exercises/reading-doc-1/equality.js
@@ -0,0 +1,22 @@
+// The equality (==) operator checks whether its two operands are equal, returning a Boolean result. 
+// It convert and compare operands that are of different types.
+let value = '15';
+console.log(value == 15); // returns true
+
+value = 15;
+console.log(value == 15); // returns true
+
+// The strict equality (===) operator checks whether its two operands are equal, returning a Boolean result.
+// The strict equality operator always considers operands of different types to be different.
+let number = 25;
+console.log(number === 25); // returns true
+
+number = '25';
+console.log(number === 25); // returns false
+
+
+
+
+
+
+

a2825a824e4c3f25f668fd49181cc53c76702d4c
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-21 21:40:25 +0430

    Date.now() static method

---
+++ js100/exercises/reading-doc-2/date.js
@@ -0,0 +1,5 @@
+// Date.now() is a method that returns the number of milliseconds since January 1, 1970:
+console.log(Date.now()); // returns 1697907897970
+
+// It's continuously updating:
+console.log(Date.now()); // returns 1697908016312

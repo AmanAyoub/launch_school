7bbc532b5dd5f0f78075e9a7b40f869b8b205de4
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-21 21:14:29 +0430

    equality operators

--- js100/exercises/reading-doc-1/equality.js
+++ js100/exercises/reading-doc-1/equality.js
@@ -1,4 +1,4 @@
-// The equality (==) operator checks whether its two operands are equal, returning a Boolean result. 
+// The equality (==) operator checks whet:her its two operands are equal, returning a Boolean result. 
 // It convert and compare operands that are of different types.
 let value = '15';
 console.log(value == 15); // returns true

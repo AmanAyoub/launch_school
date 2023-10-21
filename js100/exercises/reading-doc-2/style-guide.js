5ee27f225e9832298d8de9f8619e20d69b85c8d5
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-21 10:33:47 +0430

    coding style guide with JS

---
+++ js100/exercises/reading-doc-2/style-guide.js
@@ -0,0 +1,14 @@
+// Use camelCase for variable names:
+// End all statements with a semicolon:
+let iceCreamTaste = 'chocolate';
+let iceCreamDensity = 10;
+
+// Put the opening curly brackets of the while block on the same line as the while statement:
+while(iceCreamDensity > 0) {
+	// Indent lines with two spaces:
+	console.log('Drip...');
+	iceCreamDensity -= 1;
+
+}
+
+console.log(`The ${iceCreamTaste} ice cream melted.`);

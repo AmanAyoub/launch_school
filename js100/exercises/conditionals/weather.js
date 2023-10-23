e5bc7875b0607d8e88762312b236a6c2159b0ea0
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-23 22:09:03 +0430

    else/if statement

---
+++ js100/exercises/conditionals/weather.js
@@ -0,0 +1,13 @@
+let weather = 'snowy';
+
+if (weather === 'sunny') {
+        console.log("It's a beautiful day!");
+} else if (weather === 'rainy') {
+  console.log('Grab your umbrella.');
+} else if (weather === 'snowy') {
+        // This block of code will be executed, since weather is equal to snowy.
+        console.log("Take your jacket, it's cold!");
+} else {
+  console.log("Let's stay inside.");
+}
+

45cd10259e36980cbaa3ab72f23cf2b00026129a
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-24 20:27:29 +0430

    values that evaluates to false

--- js100/exercises/conditionals/falsy-truthy.js
+++ js100/exercises/conditionals/falsy-truthy.js
@@ -2,3 +2,4 @@
 A falsy value is something which evaluates to false, for instance when checking a variable. 
 There are only eight falsy values in JavaScript: undefined , null , NaN , 0 , "" (empty string), -0 (negative zero), 0n (BigInt zero) and false of course.
 */
+

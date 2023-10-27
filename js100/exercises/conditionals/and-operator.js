732bef5a8bf94339f3fefaf6ce58f69d9a672382
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-27 22:11:16 +0430

    (&&) and logical operator

---
+++ js100/exercises/conditionals/and-operator.js
@@ -0,0 +1,13 @@
+//  (&&) and logical operator takes two operands
+// and returns true only if both operands are true otherwise returns false:
+
+if (true && false) {
+  console.log('Yes!');
+} else {
+  console.log('No...');
+}
+// returns No...
+
+// On line 4, inside the if statement condition we used (&&) operator, since one of the operands is false
+// it returns false, so the else block will be executed.
+

198b862a157ad42d5f7d84e71de6df1abbc7b37d
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-27 21:58:27 +0430

    (||) or logical operator

---
+++ js100/exercises/conditionals/or-operator.js
@@ -0,0 +1,15 @@
+// (||) or logical operator is used with a set of operands, and it will return true if any of those operands is true, 
+// else it returns false when all the operands are not true.
+
+if (false || true) {
+        console.log('Yes!');
+} else {
+        console.log('No...');
+}
+// returns Yes!
+
+// On line 4, inside the if statement condition we used (||) operator, it takes two operands, 
+// since one operand is true it returns true,
+// so the if statement block will be executed.
+
+

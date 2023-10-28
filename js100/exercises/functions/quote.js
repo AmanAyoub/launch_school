0fdef45ceee8fff37c70279ebc6a49983220db0e
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-28 21:40:27 +0430

    return value of functions without return statement

---
+++ js100/exercises/functions/quote.js
@@ -0,0 +1,12 @@
+// Write a function that logs Brendan Eich's quote 'Always bet on JavaScript.' to the console. 
+// What is the return value of the function?
+
+function brendanEichQuote() {
+        console.log('Always bet on JavaScript.');
+}
+
+brendanEichQuote(); // logs: Always bet on JavaScript.
+
+// We didn't used return statement to return a specific value from the function, that is an explicit return value. 
+// By default, that value is undefined. So, it will return undefined implicitly.
+

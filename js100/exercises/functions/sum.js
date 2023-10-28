6e689e26676d7443b4e2171c5140f8e27b3ae1ec
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-28 21:21:01 +0430

    sum of additions

---
+++ js100/exercises/functions/sum.js
@@ -0,0 +1,9 @@
+// Write a function that takes 2 arguments and returns it's sum:
+
+function sum(num1, num2) {
+        add = num1 + num2;
+        return add;
+}
+
+console.log(sum(22, 10)); // 32
+

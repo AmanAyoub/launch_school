2fbbbef19da9f6ee4a13705598e8a85576db1fcc
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-28 21:50:06 +0430

    cite the author

---
+++ js100/exercises/functions/cite.js
@@ -0,0 +1,13 @@
+// Implement a function cite that takes two string arguments: the author of the quote and what they said. 
+// It then logs the quote to the console.
+
+function cite(author, quote) {
+        console.log(`${author} said: ${quote}`);
+}
+
+let brendanEichQuote = cite('Brendan Eich', 'Always bet on JavaScript.');
+console.log(brendanEichQuote);
+
+// logs:
+// Brendan Eich said: Always bet on JavaScript.
+

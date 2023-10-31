211218c74eec733052f5ba7be764a4a16f52d15b
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-31 20:31:19 +0430

    replacing string

---
+++ js100/exercises/functions/transformation.js
@@ -0,0 +1,9 @@
+// replace() method searches a string for a value or a regular expression.
+// The replace() method returns a new string with the value(s) replaced.
+// replace() method does not change the original string:
+
+let string = 'Captain Ruby';
+let str = string.replace('Ruby', 'JavaScript');
+
+console.log(str); // returns Captain JavaScript
+

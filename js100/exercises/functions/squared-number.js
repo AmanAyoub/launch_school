69084ea590b76aea5231216374a00e571a955b2d
Aman Ayoub <aman.ayoub16@gmail.com>
2023-10-31 19:20:54 +0430

    multiply by the power of 2

---
+++ js100/exercises/functions/squared-number.js
@@ -0,0 +1,9 @@
+// Write a function that accepts a single argument, a number, and returns the result of multiplying 
+// its argument by an exponent of 2 (also known as squaring the number).
+
+function squaredNumber(num) {
+        return num ** 2;
+}
+
+console.log(squaredNumber(3)) // 9
+

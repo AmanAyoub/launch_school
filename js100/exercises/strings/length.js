c236dc952d81335a331c834cd85f73850f3b3719
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-05 22:42:24 +0430

    checking the length of a string

---
+++ js100/exercises/strings/length.js
@@ -0,0 +1,6 @@
+// Determine the length of the string "These aren't the droids you're looking for.".
+
+let string = "These aren't the droids you're looking for.";
+
+// The length property returns the length of a string:
+console.log(string.length); // 43

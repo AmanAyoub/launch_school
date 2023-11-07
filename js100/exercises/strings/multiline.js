6ceaaaffd0c351a8d3a4b6802661bdac0b6bbf38
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-07 22:25:26 +0430

    multiline string

---
+++ js100/exercises/strings/multiline.js
@@ -0,0 +1,8 @@
+// The \n character matches newline characters.
+
+let string = 'A pirate I was meant to be!\nTrim the sails and roam the sea!'
+
+console.log(string);
+// logs:
+// A pirate I was meant to be!
+// Trim the sails and roam the sea!

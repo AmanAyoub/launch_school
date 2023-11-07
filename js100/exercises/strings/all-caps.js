aa86b905f12a777ff73762f0ae27a21054a01457
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-07 16:21:53 +0430

    toUpperCase() string method

---
+++ js100/exercises/strings/all-caps.js
@@ -0,0 +1,4 @@
+// toUpperCase() is a buil-in method that allow us to convert a JavaScript string to uppercase:
+
+string = 'confetti floating everywhere';
+console.log(string.toUpperCase()); // CONFETTI FLOATING EVERYWHERE

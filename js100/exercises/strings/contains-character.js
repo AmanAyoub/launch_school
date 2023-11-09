ac83478322a1d45814588fc735912a1ca291c619
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-09 20:09:51 +0430

    includes() string method

---
+++ js100/exercises/strings/contains-character.js
@@ -0,0 +1,7 @@
+// The includes() method returns true if a string contains a specified string. 
+// Otherwise it returns false:
+
+let byteSequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu';
+
+console.log(byteSequence.includes('x')); // true
+console.log(byteSequence.includes('v')); // false

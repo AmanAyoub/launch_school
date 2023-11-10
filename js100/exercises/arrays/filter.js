c29d47b83418b7d2cd4f14f23257e25dfbde4bb5
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-10 18:47:27 +0430

    scores above 100

---
+++ js100/exercises/arrays/filter.js
@@ -0,0 +1,6 @@
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+// The filter() method does not change the original array:
+let scores = [96, 47, 113, 89, 100, 102];
+
+let highScores = scores.filter(score => score >= 100);
+console.log(highScores.length); // 3

f623b23eccf69a205879a756c420e0bb2aa203bc
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-10 17:41:19 +0430

    split() array method

---
+++ js100/exercises/arrays/alphabet.js
@@ -0,0 +1,7 @@
+// The split() method splits a string into an array of substrings. 
+// The split() method does not change the original string:
+ 
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let characters = alphabet.split('');
+
+console.log(characters); // ['a', 'b', 'c', 'd', 'e'...]

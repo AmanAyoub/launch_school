9080346c37e34e3ff788b58584693b73e4626c8f
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-11 21:51:00 +0430

    iterating through nested arrays

---
+++ js100/exercises/arrays/vocabulary.js
@@ -0,0 +1,24 @@
+// To iterate over the vocabulary array and its sub-arrays we should write two nested for loops:
+
+let vocabulary = [
+  ['happy', 'cheerful', 'merry', 'glad'],
+  ['tired', 'sleepy', 'fatigued', 'drained'],
+  ['excited', 'eager', 'enthused', 'animated']
+];
+
+for (let i = 0; i < vocabulary.length; i++) {
+	let subArray = vocabulary[i]
+
+	for (let x = 0; x < subArray.length; x++) {
+		console.log(subArray[x]);
+	}
+}
+
+// logs:
+// happy
+// cheerful
+// merry
+// glad
+// tired
+// etc...
+

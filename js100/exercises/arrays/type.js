9f2b684bf3d483b7c19df12edff099310ca8b7b6
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-11 22:19:28 +0430

    Array.isArray() method

---
+++ js100/exercises/arrays/type.js
@@ -0,0 +1,11 @@
+// The isArray() method returns true if a variable holds an array, otherwise false:
+
+let evenNumbers = [2, 3, 4, 6];
+let games = 'Football, Cricket, and Basketball';
+
+function filter(input) {
+  return Array.isArray(input);
+}
+
+console.log(filter(evenNumbers)); // true
+console.log(filter(games)); // false

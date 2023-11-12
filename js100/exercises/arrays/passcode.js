00c88bf7ff0569e2d39f651e49c71ca2d252d2eb
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-12 14:38:46 +0430

    converting an array into a string

---
+++ js100/exercises/arrays/passcode.js
@@ -0,0 +1,9 @@
+// The join() method returns an array as a string. 
+// The join() method does not change the original array. 
+// Any separator can be specified:
+
+let passcode = ['11', 'jZ5', 'hQ3f*', '8!7g3', 'p3Fs'];
+let string = passcode.join('-');
+
+console.log(string); // 11-jZ5-hQ3f*-8!7g3-p3Fs
+ 

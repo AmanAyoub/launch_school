5d8a8e1cb3073931f839ea3083be62d7fbe18d5e
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-12 22:05:47 +0430

    accessing a variable before declaring it

---
+++ js100/exercises/variable-scope/part-1.js
@@ -0,0 +1,7 @@
+// What will the following code log to the console and why?
+
+console.log(greeting); // returns undefined
+
+var greeting = 'Hello world!';
+
+// It returns a ReferenceError, since we cannot access a variable before declaring it.

8cb810c12e0d7d7e1c5a5c738e05fd016c93aeb6
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-18 11:54:42 +0430

    scope of variables declared with let keyword

---
+++ js100/exercises/variable-scope/part-2.js
@@ -0,0 +1,8 @@
+// What will the following code log to the console and why?
+
+// It returns undefined
+console.log(greeting);
+
+let greeting = 'Hello world!'; // ReferenceError: Cannot access 'greeting' before initialization
+
+// It returns ReferenceError since we cannot access variables declared with let keyword before declaring it.

4098f895e37672ef9d2bae92c770e53d45abee88
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-20 18:20:42 +0430

    accessing global variable inside a block

---
+++ js100/exercises/variable-scope/part-7.js
@@ -0,0 +1,11 @@
+// What will the following code log to the console and why?  
+
+let a = 1;
+
+function myFunction() {
+  console.log(a);
+}
+
+console.log(myFunction()); // logs: 1
+
+// Since a is a global variable so we can access it anywhere through program even inside a block.

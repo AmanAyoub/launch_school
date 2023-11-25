23f2a6237aab151834210f4c29f958571f9539a9
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-25 15:20:54 +0430

    decreasing value with for loop

---
+++ js100/exercises/loops/countdown.js
@@ -0,0 +1,14 @@
+// Write code that logs the numbers from 10 to 1 in decreasing order, and then logs 'Launch!'.
+
+for (let i = 10; i > 0; i--) {
+	console.log(i);
+}
+console.log('Launch!');
+
+// The above code decreases 1 from i local variable in each iteration until i is greater than 0.
+// logs:
+// 10
+// 9
+// 8
+// 7...
+// Launch!

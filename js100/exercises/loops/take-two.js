09dc6d3293dda1fa7b326802bfc4aa51fe92b3f5
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-28 19:59:39 +0430

    multiplying each element by 2

---
+++ js100/exercises/loops/take-two.js
@@ -0,0 +1,22 @@
+// Write a for loop that iterates over all numbers from 1 to 100, 
+// and outputs the result of multiplying each element by 2.
+
+// we can do so by using for or while loops:
+for (let i = 1; i <= 100; i++) {
+	console.log(i * 2);
+}
+// logs:
+// 2
+// 4
+// 6
+// 8
+// 10...
+
+
+// Using while loop:
+let num = 1;
+while (num <= 100) {
+	console.log(num * 2);
+	num += 1;
+}
+// Same output

30baa22285af3e19681ebbd68b7537cb83804e18
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-28 19:01:03 +0430

    log the message 3 time

---
+++ js100/exercises/loops/triple-greeting.js
@@ -0,0 +1,13 @@
+// Write a loop that logs greeting three times.
+
+// We can do so by writing a for loop:
+let greeting = 'Aloha!';
+
+for (let i = 0; i < 3; i++) {
+	console.log(greeting)
+}
+// logs:
+// Aloha!
+// Aloha!
+// Aloha!
+

7231464cf2c8176a5582de14b1682926af11fe7e
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-28 20:13:28 +0430

    looping over array elements

---
+++ js100/exercises/loops/array-element.js
@@ -0,0 +1,16 @@
+// write a while loop that logs the elements of array at each index, and terminates 
+// after logging the last element of the array.
+
+let array = [1, 2, 3, 4];
+let index = 0;
+
+while (index < array.length) {
+	console.log(array[index]);
+	index += 1;
+}
+// logs:
+// 1
+// 2
+// 3
+// 4
+

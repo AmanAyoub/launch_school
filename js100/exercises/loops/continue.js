d078a94fd6e4642c3de9ae1a2324292fe599c377
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-28 20:29:38 +0430

    log the length of array elements

---
+++ js100/exercises/loops/continue.js
@@ -0,0 +1,21 @@
+// write a for loop that loops over the elements of the array cities and logs the length of each string to the console. 
+// If the element is null, skip forward to the next iteration without logging anything to the console.
+let cities = ['Istanbul', 'Los Angeles', 'Tokyo', null, 'Vienna', null, 'London', 'Beijing', null];
+
+for (let i = 0; i < cities.length; i++) {
+	let element = cities[i];
+	
+	if (element === null) {
+		continue;
+	}
+	console.log(element.length);
+}
+// logs:
+// 8
+// 11
+// 5
+// 6
+// 6
+// 7
+
+	

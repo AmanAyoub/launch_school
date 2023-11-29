9da37adc3b296e3ec65550033f7dab9950fb0d0c
Aman Ayoub <aman.ayoub16@gmail.com>
2023-11-29 20:26:56 +0430

    difference btw do while & while loop

---
+++ js100/exercises/loops/do-while.js
@@ -0,0 +1,43 @@
+// What's the difference between do while and while loop?
+
+// The while loop loops through a block of code as long as a specified condition is true:
+let counter = 0;
+
+while (counter > 0) {
+  console.log('Woooot!');
+  counter -= 1;
+}
+
+
+// The do...while statements defines a code block to be executed once, 
+// and then repeat as long as the condition is true.
+// The do...while is used when you want to run a code block at least one time:
+let counter = 0;
+
+do {
+  console.log('Woooot!');
+  counter -= 1;
+} while (counter > 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

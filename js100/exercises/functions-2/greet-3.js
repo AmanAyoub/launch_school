2fa3e8a8b34809e159c20992d7b4642578b9c3a0
Aman Ayoub <aman.ayoub16@gmail.com>
2023-12-06 12:24:42 +0430

    greet function without parameters

---
+++ js100/exercises/functions-2/greet-3.js
@@ -0,0 +1,21 @@
+// Change the function so that it doesn't take any arguments, 
+// and instead calls the functions greeting and recipient defined below:
+
+function greeting() {
+  return 'Good morning';
+}
+
+function recipient() {
+  return 'Launch School';
+}
+
+
+function greet() {
+	return greeting() + ', ' + recipient();
+}
+
+console.log(greet()); // Good morning, Launch School
+
+
+
+

645dd555f29847386e1cddd09ae95a0406e2951f
Aman Ayoub <aman.ayoub16@gmail.com>
2024-03-09 10:10:35 +0430

    removing the last character of strings

--- js100/exercises/functions-2/remove_last_char.js
+++ js100/exercises/functions-2/remove_last_char.js
@@ -2,8 +2,13 @@
 // and returns the string without the last character.
 
 function removeLastChar(str) {
-  return str.slice(0, str.length-1);
+  return str.substr(0, str.length - 1);
 }
 
 console.log(removeLastChar("Hello world!")); // Hello World
 console.log(removeLastChar("Heyy")); // Hey
+
+// On line 5, the substr() method extracts a part of a string.
+// The substr() method begins at a specified position,
+// and returns a specified number of characters.
+// The substr() method does not change the original string.

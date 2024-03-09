f482478ca00fcd99316a7bb21e7c2420764dde98
Aman Ayoub <aman.ayoub16@gmail.com>
2024-03-09 16:01:07 +0430

    accessing the properties of an object

---
+++ js100/exercises/objects/retrieve_value.js
@@ -0,0 +1,19 @@
+// Write the code necessary to retrieve the value of the
+// courses property of our student object.
+
+let student = {
+  name: 'Carmen',
+  age: 14,
+  grade: 10,
+  courses: ['biology', 'algebra', 'composition', 'ceramics'],
+  gpa: 3.75,
+};
+
+// We can access a specific property of an object
+// using dot notation or bracket notation.
+
+// Dot notation:
+console.log(student.courses); // [ 'biology', 'algebra', 'composition', 'ceramics' ]
+
+// Bracket notation:
+console.log(student.courses); // [ 'biology', 'algebra', 'composition', 'ceramics' ]

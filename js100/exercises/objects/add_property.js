652045a1928eb8e41e7b70bb2dfc4447b79264c3
Aman Ayoub <aman.ayoub16@gmail.com>
2024-03-10 08:59:36 +0430

    adding properties to objects

---
+++ js100/exercises/objects/add_property.js
@@ -0,0 +1,12 @@
+let fido = {
+  name: 'Fido',
+  species: 'Labrador Retriever',
+  color: 'brown',
+  weight: 16,
+};
+
+// Dot notation:
+fido.age = 4;
+
+// Bracket notation:
+fido['favorite food'] = "Oreo";

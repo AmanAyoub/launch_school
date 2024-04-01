c4c83412ce92d48bfa17605a9916d3e32e73e2ac
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-01 18:10:07 +0430

    copying objects

---
+++ js100/exercises/objects/cloning_person.js
@@ -0,0 +1,26 @@
+function clone(object) {
+  let copy = {};
+  for (let key in object) {
+    copy[key] = object[key];
+  }
+  return copy;
+}
+
+let person = {
+  title: 'Duke',
+  name: {
+    value: 'Nukem',
+    isStageName: true
+  },
+  age: 33
+};
+
+let clonedPerson = clone(person);
+person.age = 34;
+
+console.log(person.age);       // 34
+console.log(clonedPerson.age); // 33
+
+person.name.isStageName = false;
+console.log(person.name.isStageName);       // false
+console.log(clonedPerson.name.isStageName); // false

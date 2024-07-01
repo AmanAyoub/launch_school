dff9d95d553ac9976c1d20aae634053d1f4a961c
Aman Ayoub <aman.ayoub16@gmail.com>
2024-07-02 04:00:30 +0430

    reverse integers

---
+++ JS110/exercises/easy_6/reverse_number.js
@@ -0,0 +1,13 @@
+// Write a function that takes a positive integer as an argument
+// and returns that number with its digits reversed.
+
+
+function reverseNumber(number) {
+  return Number(number.toString().split('').reverse().join(''));
+}
+
+console.log(reverseNumber(12345));    // 54321
+console.log(reverseNumber(12213));    // 31221
+console.log(reverseNumber(456));      // 654
+console.log(reverseNumber(12000));    // 21 -- Note that leading zeros in the result get dropped!
+console.log(reverseNumber(1));        // 1

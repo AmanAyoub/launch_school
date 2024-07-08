f77ad63e66e93259147df35524e5cb7656cdd8dc
Aman Ayoub <aman.ayoub16@gmail.com>
2024-07-08 09:00:00 +0430

    multiplicative average

--- JS110/exercises/easy_5/multiplicative_average.js
+++ JS110/exercises/easy_5/multiplicative_average.js
@@ -5,6 +5,6 @@
 function multiplicativeAverage(array) {
   return (array.reduce((acc, num) => acc * num) / array.length).toFixed(3);
 }
-
+ 
 console.log(multiplicativeAverage([3, 5]));                   // "7.500"
 console.log(multiplicativeAverage([2, 5, 7, 11, 13, 17]));    // "28361.667"

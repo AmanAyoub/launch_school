4883aa95f458453c4410da054a156bad354e9186
Aman Ayoub <aman.ayoub16@gmail.com>
2024-07-09 05:00:00 +0430

    multiplay list

--- JS110/exercises/easy_5/multiply_lists.js
+++ JS110/exercises/easy_5/multiply_lists.js
@@ -7,6 +7,6 @@ function multiplyList(arr1, arr2) {
     acc.push((arr1[index] * arr2[index]));
     return acc;
     }, []);
-}
+} 
 
 console.log(multiplyList([3, 5, 7], [9, 10, 11]));    // [27, 50, 77]

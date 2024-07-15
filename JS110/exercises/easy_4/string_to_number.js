6c3c07b3a122c84377c606314c3d49012ea94b34
Aman Ayoub <aman.ayoub16@gmail.com>
2024-07-15 10:33:57 +0430

    convert a string to a number

---
+++ JS110/exercises/easy_4/string_to_number.js
@@ -0,0 +1,11 @@
+// Write a function that takes a string of digits and returns the appropriate number as an integer.
+// You may not use any of the methods mentioned above.
+
+
+function stringToInteger(string) {
+  return string - 0;
+}
+
+
+console.log(stringToInteger("4321") === 4321); // logs true
+console.log(stringToInteger("570") === 570); // logs true

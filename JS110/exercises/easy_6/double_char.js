a7df00b67a542bf5d9998e8b48b5f31b93b23f15
Aman Ayoub <aman.ayoub16@gmail.com>
2024-07-17 10:59:45 +0430

    double char (part 1)

---
+++ JS110/exercises/easy_6/double_char.js
@@ -0,0 +1,11 @@
+// Write a function that takes a string, doubles every character in the string, and returns the result as a new string.
+
+
+function repeater(string) {
+  return string.split('').map(char => char + char).join('');
+}
+
+
+console.log(repeater('Hello'));        // "HHeelllloo"
+console.log(repeater('Good job!'));    // "GGoooodd  jjoobb!!"
+console.log(repeater(''));             // ""

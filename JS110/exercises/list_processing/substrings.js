c008043bb36f67f3c909e2e5d9ac33ceba4a769a
Aman Ayoub <aman.ayoub16@gmail.com>
2024-07-29 03:00:00 +0430

    leading substring

--- JS110/exercises/list_processing/substrings.js
+++ JS110/exercises/list_processing/substrings.js
@@ -31,14 +31,22 @@ A
   - Return `substrings`
 */
 
+function leadingSubstrings(string) {
+  let substrings = [];
+  for (let index = 1; index <= string.length; index++) {
+    substrings.push(string.slice(0, index));
+  }
+
+  return substrings;
+}
+
 // Further exploration:
 function leadingSubstrings2(string) {
-  let characters = string.split('')
-  let substrings = characters.map((char, index) => characters.slice(0, index + 1).join(''));
+  let substrings = string.split('').map((_, index) => string.slice(0, index + 1));
 
   return substrings;
 }
 
 console.log(leadingSubstrings2('abc'));      // ["a", "ab", "abc"]
 console.log(leadingSubstrings('a'));        // ["a"]
-console.log(leadingSubstrings('xyzzy'));    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
+console.log(leadingSubstrings2('xyzzy'));    // ["x", "xy", "xyz", "xyzz", "xyzzy"]

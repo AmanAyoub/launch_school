79acd8d10cc41bed9c7ca6e7e037a4ecde597209
AmanAyoub <aman.ayoub16@gmail.com>
2024-08-03 06:07:37 +0430

    next featured number

--- JS110/exercises/medium_2/featured_num.js
+++ JS110/exercises/medium_2/featured_num.js
@@ -1,7 +1,10 @@
 /*
-A featured number (something unique to this exercise) is an odd number that is a multiple of 7, with all of its digits occurring exactly once each. For example, 49 is a featured number, but 98 is not (it is not odd), 97 is not (it is not a multiple of 7), and 133 is not (the digit 3 appears twice).
+A featured number (something unique to this exercise) is an odd number that is a multiple of 7,
+with all of its digits occurring exactly once each. For example, 49 is a featured number,
+but 98 is not (it is not odd), 97 is not (it is not a multiple of 7), and 133 is not (the digit 3 appears twice).
 
-Write a function that takes an integer as an argument and returns the next featured number greater than the integer. Issue an error message if there is no next featured number.
+Write a function that takes an integer as an argument and returns the next featured number greater than the integer.
+Issue an error message if there is no next featured number.
 
 NOTE: The largest possible featured number is 9876543201.
 

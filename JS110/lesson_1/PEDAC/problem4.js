48e3d3307af3cd5d4a1a36a7a4a719328f1f8d29
AmanAyoub <aman.ayoub16@gmail.com>
2024-08-04 14:11:39 +0430

    missing numbers

--- JS110/lesson_1/PEDAC/problem4.js
+++ JS110/lesson_1/PEDAC/problem4.js
@@ -8,9 +8,39 @@ The number 1 should be in the input array, if it's not present in the input arra
 it should be included in the results. See the example below.
 
 [8, 10, 11, 7, 3, 15, 6, 14, 5, 12]  ---> [1, 2, 4, 9, 13]
+
+P:
+In: array
+Out: array (not the same array) 
+Rules:
+ - Make a list of missing numbers
+ - Number starts from 1
+ - Input array length doesn't matter
+ - Input array won't contain duplicate numbers
+
+[3, 2, 5] ==> [1, 4]
+
+D:
+ - Array: to store the missing numbers
+
+A:
+1. Create an empty array missingNums
+2. Get the highest number from the input array
+3. Iterate through numbers from 1 to the highest number
+ - If current number is not included in the input array
+  - Push it to missingNums
+4. Return missingNums
 */
 
+function missNumsFinder(numbers) {
+  let missingNums = [];
+  let highestNum = Math.max(...numbers);
+  for (let currentNum = 1; currentNum < highestNum; currentNum++) {
+    if (! numbers.includes(currentNum)) missingNums.push(currentNum);
+  }
 
+  return missingNums;
+}
 
 console.log(missNumsFinder([8, 10, 11, 7, 3, 15, 6, 14, 5, 12])); // [1, 2, 4, 9, 13]
 console.log(missNumsFinder([2, 3, 1, 9, 4, 5, 6, 10, 7])); // [8]

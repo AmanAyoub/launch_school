d855cfbe0e42462e1898fed49466f10078732706
AmanAyoub <aman.ayoub16@gmail.com>
2024-08-09 01:05:37 +0430

    cute angles

--- JS110/exercises/easy_5/cute_angles.js
+++ JS110/exercises/easy_5/cute_angles.js
@@ -1,5 +1,8 @@
 /*
-Write a function that takes a floating point number representing an angle between 0 and 360 degrees and returns a string representing that angle in degrees, minutes, and seconds. You should use a degree symbol (˚) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
+Write a function that takes a floating point number representing an angle between 0
+and 360 degrees and returns a string representing that angle in degrees, minutes, and seconds.
+You should use a degree symbol (˚) to represent degrees, a single quote (') to represent minutes,
+and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
 
 In: number
 Out: string

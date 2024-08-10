8a013222fd0db75c73216f260e30023d9090177f
AmanAyoub <aman.ayoub16@gmail.com>
2024-08-10 15:06:32 +0430

    doubled consonants

--- JS110/exercises/easy_6/double_char_2.js
+++ JS110/exercises/easy_6/double_char_2.js
@@ -1,17 +1,34 @@
 /*
-Write a function that takes a string, doubles every consonant character in the string,
-and returns the result as a new string. The function should not double vowels ('a','e','i','o','u'),
-digits, punctuation, or whitespace.
+Write a function that takes a string, doubles every consonant character in the string, and returns the result as a new string. The function should not double vowels ('a','e','i','o','u'), digits, punctuation, or whitespace.
+
+In: string
+Out: string
+Rules:
+ - Do not double vowels, digits, punctuation, or whitespace
+ - Double consonant characters
+
+D:
+ - Array: to store the characters
+
+A:
+1. Convert the input string to an array of characters
+2. Iterate through characters
+ - If the current character is a consonant
+  - Double it
+3. Convert the array to a string, return it
 */
 
 function doubleConsonants(string) {
-  let doubledCharacters = string.split('').map(char => {
-    if (char.match(/\w/g) && !['a', 'e', 'i', 'o', 'u'].includes(char)) return char.repeat(2);
+  let characters = string.split('');
+  let doubledConsonants = characters.map((char) => {
+    if (char.match(/[^aeiou0-9_\W]/gi)) return char + char;
     return char;
   });
 
-  return doubledCharacters.join('');
+  return doubledConsonants.join('');
 }
+
+
 
 console.log(doubleConsonants('String'));          // "SSttrrinngg"
 console.log(doubleConsonants('Hello-World!'));    // "HHellllo-WWorrlldd!"

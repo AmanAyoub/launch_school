c9d1913816e141b510a6e0208addcbc7f2347a64
AmanAyoub <aman.ayoub16@gmail.com>
2024-08-12 14:52:04 +0430

    missing values

--- JS110/lesson_1/PEDAC/problem4.js
+++ JS110/lesson_1/PEDAC/problem4.js
@@ -9,33 +9,27 @@ it should be included in the results. See the example below.
 
 [8, 10, 11, 7, 3, 15, 6, 14, 5, 12]  ---> [1, 2, 4, 9, 13]
 
-P:
 In: array
 Out: array (not the same array) 
 Rules:
- - Make a list of missing numbers
- - Number starts from 1
- - Input array length doesn't matter
- - Input array won't contain duplicate numbers
-
-[3, 2, 5] ==> [1, 4]
-
-D:
- - Array: to store the missing numbers
+ - Input array contains unique values
+ - Find the missing values 
+ - Input array is not in ascending order
+ - Output array should be in ascending order
+ - The minimum number should be 1
 
 A:
-1. Create an empty array missingNums
-2. Get the highest number from the input array
-3. Iterate through numbers from 1 to the highest number
- - If current number is not included in the input array
-  - Push it to missingNums
-4. Return missingNums
+1. Get the maximum number from input array
+2. Iterate through numbers from 1 to maximum
+ - If the current number is not included in the input array
+  - Store the current number as the missing value
+3. Return the missing values
 */
 
 function missNumsFinder(numbers) {
   let missingNums = [];
-  let highestNum = Math.max(...numbers);
-  for (let currentNum = 1; currentNum < highestNum; currentNum++) {
+  let maxNum = Math.max(...numbers);
+  for (let currentNum = 1; currentNum < maxNum; currentNum++) {
     if (! numbers.includes(currentNum)) missingNums.push(currentNum);
   }
 
@@ -48,4 +42,3 @@ console.log(missNumsFinder([5, 4, 2, 9, 3, 8, 10, 6, 7])); //[1]
 console.log(missNumsFinder([7, 1, 12, 9, 11, 14, 13, 6, 10, 5])); // [2, 3, 4, 8]
 console.log(missNumsFinder([8, 10, 11, 7, 3, 15, 6, 1, 14, 5, 12])); // [2, 4, 9, 13]
 console.log(missNumsFinder([9, 10, 7, 2, 11, 8, 1, 17, 6, 16, 18, 19, 15, 3, 13])); //[4, 5, 12, 14]
-

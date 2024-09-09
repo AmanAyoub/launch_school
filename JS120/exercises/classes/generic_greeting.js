7ce0ec68dc785b3ddcd8787c72bd836d4872e0fc
AmanAyoub <aman.ayoub16@gmail.com>
2024-09-09 22:52:10 +0430

    static

---
+++ JS120/exercises/classes/generic_greeting.js
@@ -0,0 +1,13 @@
+// Modify the following code so that Hello! I'm a cat! is logged
+// when Cat.genericGreeting is invoked.
+
+class Cat {
+  static genericGreeting() {
+    console.log("Hello! I'm a cat!");
+  }
+}
+
+Cat.genericGreeting();
+
+// Expected output:
+// Hello! I'm a cat!

bf2e0fdf85943730949f78549981e0ddadc943de
AmanAyoub <aman.ayoub16@gmail.com>
2024-09-22 17:01:34 +0430

    longest vowel substring

--- JS110/lesson_1/PEDAC/problem16.js
+++ JS110/lesson_1/PEDAC/problem16.js
@@ -1,5 +1,8 @@
 /*
-Create a function that takes a non-empty string as an argument. The string consists entirely of lowercase alphabetic characters. The function should return the length of the longest vowel substring. The vowels of interest are "a", "e", "i", "o", and "u".
+Create a function that takes a non-empty string as an argument.
+The string consists entirely of lowercase alphabetic characters.
+The function should return the length of the longest vowel substring.
+The vowels of interest are "a", "e", "i", "o", and "u".
 
 P:
 In: a string

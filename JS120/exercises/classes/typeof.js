cf808d924521e9652188988d2fc9f35ccc99fa68
AmanAyoub <aman.ayoub16@gmail.com>
2024-09-23 22:43:00 +0430

    what are you?

---
+++ JS120/exercises/classes/typeof.js
@@ -0,0 +1,11 @@
+// Using the code from the previous exercise, 
+// add a constructor method that logs to the console I'm a cat!
+// when a new Cat object is initialized.
+
+class Cat {
+  constructor() {
+    console.log("I'm a cat!");
+  }
+}
+
+let kitty = new Cat();

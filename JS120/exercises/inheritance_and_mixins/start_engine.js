75300595d62638cefe1dd16b7cd43f0bb7f3b146
AmanAyoub <aman.ayoub16@gmail.com>
2024-10-23 18:28:10 +0430

    vehicle & truck

---
+++ JS120/exercises/inheritance_and_mixins/start_engine.js
@@ -0,0 +1,25 @@
+// Given the following code, modify the Truck class so that the code shown below displays the indicated output.
+// Your code should make use of the startEngine method in the Vehicle class.
+
+
+class Vehicle {
+  startEngine() {
+    return 'Ready to go!';
+  }
+}
+
+class Truck extends Vehicle {
+  startEngine(speed) {
+    return `${super.startEngine()} Drive ${speed}, please!`;
+  }
+}
+
+let truck1 = new Truck();
+console.log(truck1.startEngine('fast'));
+
+let truck2 = new Truck();
+console.log(truck2.startEngine('slow'));
+
+// Expected output:
+// Ready to go! Drive fast, please!
+// Ready to go! Drive slow, please!

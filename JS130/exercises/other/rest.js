41dbebc5864f97781889d2d3757ee6e06042ee34
AmanAyoub <aman.ayoub16@gmail.com>
2024-11-08 20:13:42 +0430

    rest syntax

---
+++ JS130/exercises/other/rest.js
@@ -0,0 +1,9 @@
+// How can we refactor the function definition below (without changing the function invocation) so that we don't need to use the arguments object?
+
+function sum(...values) {
+  return values.reduce(function(a, b) {
+    return a + b;
+  });
+}
+
+console.log(sum(1, 4, 5, 6)); // 16

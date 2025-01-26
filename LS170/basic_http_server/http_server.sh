d0f049613609ec336c594cafce600d6839085a7c
AmanAyoub <aman.ayoub16@gmail.com>
2025-01-26 09:54:57 +0430

    200 OK or 400 Bad Request

--- LS170/basic_http_server/http_server.sh
+++ LS170/basic_http_server/http_server.sh
@@ -3,8 +3,13 @@
 function server () {
   while true
   do
-    read message
-    echo You said: $message
+    read method path version
+    if [[ $method = 'GET' ]]
+    then
+      echo 'HTTP/1.1 200 OK'
+    else
+      echo 'HTTP/1.1 400 Bad Request'
+    fi
   done
 }
 

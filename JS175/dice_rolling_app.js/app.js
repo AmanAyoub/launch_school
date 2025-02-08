b7faedcda510c4fd7721fe51f51b3a35bc96f031
AmanAyoub <aman.ayoub16@gmail.com>
2025-02-08 16:07:30 +0430

    die roll

--- JS175/dice_rolling_app.js/app.js
+++ JS175/dice_rolling_app.js/app.js
@@ -1,22 +1,41 @@
 const HTTP = require('http');
 const PORT = 3000;
+const URL = require("url").URL;
+
 
 function diceRoll(min, max) {
   return Math.floor(Math.random() * max) + min;
 }
 
+function rollDice(params) {
+  let sides = params.get('sides');
+  let rolls = params.get('rolls');
+  let body = '';
+  while (rolls !== 0) {
+    body += `${diceRoll(1, sides)}\n`;
+    rolls -= 1;
+  }
+
+  return body;
+}
+
+function getParams(path) {
+  let myUrl = new URL(path, `http://localhost:${PORT}`);
+  return myUrl.searchParams;
+}
+
 const SERVER = HTTP.createServer((req, res) => {
   let method = req.method;
   let path = req.url;
-  if (path === 'favicon.ico') {
+
+  if (path === '/favicon.ico') {
     res.statusCode = 404;
     res.end();
   } else {
-    let diceValue = diceRoll(1, 6);
-
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.write(`${diceValue}\n`);
+    let content = rollDice(getParams(path));
+    res.write(`${content}\n`);
     res.write(`${method} ${path}`);
     res.end();
   }

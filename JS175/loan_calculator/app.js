185b0dfc473be5782875ee930f085ea25c762689
AmanAyoub <aman.ayoub16@gmail.com>
2025-02-09 18:54:02 +0430

    lock

--- JS175/loan_calculator/app.js
+++ JS175/loan_calculator/app.js
@@ -1,14 +1,14 @@
 const HTTP = require('http');
 const URL = require('url').URL;
-const ARP = 5;
-let PORT = 4000;
-const HTML_START = `
+const PORT = 3000;
+
+const TEMPLATE = `
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Loan Calculator</title>
-        <style type="text/css">
+    <style type="text/css">
       body {
         background: rgba(250, 250, 250);
         font-family: sans-serif;
@@ -28,119 +28,102 @@ const HTML_START = `
       }
 
       table {
-        font-size: 2rem;
+        font-size: 1.5rem;
       }
-
       th {
         text-align: right;
+      }
+      td {
+        text-align: center;
+      }
+      th,
+      td {
+        padding: 0.5rem;
       }
     </style>
   </head>
   <body>
-    <article><tr>
-  <th>Amount:</th>
-  <td>
-    <a href='/?amount=1800&duration=5'>- $100</a>
-  </td>
-  <td>$1900</td>
-  <td>
-    <a href='/?amount=2000&duration=5'>+ $100</a>
-  </td>
-</tr>
-<tr>
-  <th>Duration:</th>
-  <td>
-    <a href='/?amount=1900&duration=4'>- 1 year</a>
-  </td>
-  <td>5 years</td>
-  <td>
-    <a href='/?amount=1900&duration=6'>+ 1 year</a>
-  </td>
-</tr>
-<tr>
-  <th>APR:</th>
-  <td colspan='3'>5%</td>
-</tr>
-<tr>
-  <th>Monthly payment:</th>
-  <td colspan='3'>$35.86</td>
-</tr>
+    <article>
       <h1>Loan Calculator</h1>
       <table>
-        <tbody>`;
-
-const HTML_END = `
+        <tbody>
+          <tr>
+            <th>Amount:</th>
+            <td>
+              <a href='/?amount=${amount - 100}&duration=${duration}'>- $100</a>
+            </td>
+            <td>$${amount}</td>
+            <td>
+              <a href='/?amount=${amount + 100}&duration=${duration}'>+ $100</a>
+            </td>
+          </tr>
+          <tr>
+            <th>Duration:</th>
+            <td>
+              <a href='/?amount=${amount}&duration=${duration -1}'>- 1 year</a>
+            </td>
+            <td>${duration} years</td>
+            <td>
+              <a href='/?amount=${amount}&duration=${duration + 1}'>+ 1 year</a>
+            </td>
+          </tr>
+          <tr>
+            <th>APR:</th>
+            <td colspan='3'>${APR}%</td>
+          </tr>
+          <tr>
+            <th>Monthly payment:</th>
+            <td colspan='3'>$${payment}</td>
+          </tr>
         </tbody>
       </table>
     </article>
   </body>
-</html>`;
+</html>
+`;
 
 function getParams(path) {
-  return (new URL(path, `http://localhost:${PORT}`)).searchParams;
-}
+  const myURL = new URL(path, `http://localhost:${PORT}`);
+  return myURL.searchParams;
+};
 
-function monthlyPayment(amount, duration) {
-  let monthlyDuration = duration * 12;
-  let monthlyARP = ARP / 100 / 12;
-  let monthlyPayment = amount * (monthlyARP / (1 - Math.pow((1 + monthlyARP), (-monthlyDuration))));
-  return monthlyPayment;
-}
+function calculateLoan(amount, duration, apr) {
+  let annualInterestRate = apr / 100;
+  let monthlyInterestRate = annualInterestRate / 12;
+  let months = Number(duration) * 12;
+  let payment = amount *
+          (monthlyInterestRate /
+          (1 - Math.pow((1 + monthlyInterestRate),(-months))));
 
-function createBody(params) {
+  return payment.toFixed(2);
+};
+
+function createLoanOffer(params) {
+  const APR = 5;
   let amount = Number(params.get('amount'));
   let duration = Number(params.get('duration'));
-  let paymentPerMonth = monthlyPayment(amount, duration).toFixed(2);
-  let body = HTML_START + `<tr>
-  <th>Amount:</th>
-    <td>
-      <a href='/?amount=${amount - 100}&duration=${duration}'>- $100</a>
-    </td>
-    <td>$${amount}</td>
-    <td>
-      <a href='/?amount=${amount + 100}&duration=${duration}'>+ $100</a>
-    </td>
-  </tr>
-  <tr>
-    <th>Duration:</th>
-    <td>
-      <a href='/?amount=${amount}&duration=${duration - 1}'>- 1 year</a>
-    </td>
-    <td>${duration} years</td>
-    <td>
-      <a href='/?amount=${amount}&duration=${duration + 1}'>+ 1 year</a>
-    </td>
-  </tr>
-  <tr>
-    <th>APR:</th>
-    <td colspan='3'>${ARP}%</td>
-  </tr>
-  <tr>
-    <th>Monthly payment:</th>
-    <td colspan='3'>$${paymentPerMonth}</td>
-  </tr>` + HTML_END;
-  return body;
-}
+  let payment = calculateLoan(amount, duration, APR);
 
+  return TEMPLATE;
+};
 
-const myServer = HTTP.createServer((req, res) => {
+const SERVER = HTTP.createServer((req, res) => {
+  let method = req.method;
   let path = req.url;
 
-  if (path === "/favicon.ico") {
-    res.statusCode = 404;
+  if (path === '/favicon.ico') {
+    res.statusCode = 400;
     res.end();
   } else {
-    let params = getParams(path);
-    let content = createBody(params);
+    let content = createLoanOffer(getParams(path));
 
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-
+    res.setHeader('Content-Type', 'text/html');
     res.write(`${content}\n`);
     res.end();
   }
 });
 
-myServer.listen(PORT, () => {
-  console.log(`Listeing on port ${PORT}...`);
+SERVER.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });

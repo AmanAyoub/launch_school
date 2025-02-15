cccb692e1d90e0809a0e19e1bf74ae94c8dcf273
AmanAyoub <aman.ayoub16@gmail.com>
2025-02-15 12:09:19 +0430

    contacts manager

---
+++ JS175/contacts/contacts.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+let contactData = [
+  {
+    firstName: "Mike",
+    lastName: "Jones",
+    phoneNumber: "281-330-8004"
+  },
+  {
+    firstName: "Jenny",
+    lastName: "Keys",
+    phoneNumber: "768-867-5309",
+  },
+  {
+    firstName: "Max",
+    lastName: "Entiger",
+    phoneNumber: "214-748-3647",
+  },
+  {
+    firstName: "Alicia",
+    lastName: "Keys",
+    phoneNumber: "515-489-4608",
+  },
+];
+
+const sortContacts = contacts => {
+  return contacts.slice().sort((contactA, contactB) => {
+    if (contactA.lastName < contactB.lastName) {
+      return -1;
+    } else if (contactA.lastName > contactB.lastName) {
+      return 1;
+    } else if (contactA.firstName < contactB.firstName) {
+      return -1;
+    } else if (contactA.firstName > contactB.firstName) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.redirect("/contacts");
+});
+
+app.get("/contacts", (req, res) => {
+  res.render("contacts", {
+    contacts: sortContacts(contactData),
+  });
+});
+
+app.get("/contacts/new", (req, res) => {
+  res.render("new-contact");
+});
+
+app.listen(4500, "localhost", () => {
+  console.log("Listening to port 4500.");
+});

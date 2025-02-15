9b8485c9e543b2b527ef0bb8cb6df9d1f3aa1a3b
AmanAyoub <aman.ayoub16@gmail.com>
2025-02-15 08:08:48 +0430

    view variables

--- JS175/hello-world/hello.js
+++ JS175/hello-world/hello.js
@@ -14,24 +14,32 @@ app.get('/', (req, res) => {
 });
 
 app.get("/english", (req, res) => {
-  res.render("hello-world-english");
+  res.render("hello-world-english", {
+    "currentLinkIsEnglish": "current"
+  });
 });
 
 app.get("/french", (req, res) => {
-  res.render("hello-word-french");
+  res.render("hello-word-french", {
+    "currentLinkIsFrench": "current"
+  });
 });
 
 
 app.get("/pashto", (req, res) => {
-  res.render("hello-word-pashto");
+  res.render("hello-word-pashto", {
+    "currentLinkIsPashto": "current"
+  });
 });
 
 app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian");
+  res.render("hello-world-serbian", {
+    "currentLinkIsSerbian": "current"
+  });
 });
 
-app.listen(4000, "localhost", () => {
-  console.log("Listeing to port 4000.");
+app.listen(5000, "localhost", () => {
+  console.log("Listeing to port 5000.");
 });
 
 
